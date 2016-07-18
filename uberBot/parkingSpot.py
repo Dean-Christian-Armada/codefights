12e05b5756e50e6b9ee02cfb5b85ae56f2c87793
Dean Christian Armada <deanarmada@gmail.com>
2016-07-18 14:45:51 +0800

    UberBot Defeated Question Set 1

--- uberBot/parkingSpot.py
+++ uberBot/parkingSpot.py
@@ -3,147 +3,121 @@ def parkingSpot(carDimensions, parkingLot, luckySpot):
 	l = cD[0] # length
 	w = cD[1] # width 
 	lS = luckySpot
+	# This method is used to make sure if the luckySpot is compatible with carDimensions
 	def isParkable(lS, cD):
 		if(lS[2] - lS[0] + 1 == cD[0] and lS[3] - lS[1] + 1 == cD[1]):
-			return True
+			cond = 1
+			return (True, cond)
 		elif(lS[2] - lS[0] + 1 == cD[1] and lS[3] - lS[1] + 1 == cD[0]):
-			return True
+			cond = 2
+			return (True, cond)
 		else:
-			return False
+			return (False, False)
 
-	if isParkable(lS, carDimensions):
-		numOfSpots = l * w
-		spots = []
-		spots.append([lS[0], lS[1]])
-		spots.append([lS[2], lS[3]])
+	iP = isParkable(lS, carDimensions)
 
-		_x = (lS[2]-lS[0]) + 1
-		_y = (lS[3]-lS[1]) + 1
+	if iP[0]: # If luckySpot is compatible with carDimensions
+		spots = [] # This is where all the parking coordinates will be stored
 
-		count = 1
+		# Determines the parking coordinates for the carDimension
+		# Please be informed that luckySpot plays a great role on determining the parking coordinates
+		# The trick is the first two indexes is actually the [x, y] of the starting coordinate of the parking
+		# And the last two indexes is actullay the [x, y] of the last coordinate of the parking
+		for x in range(lS[0], lS[2]+1):
+			for y in range(lS[1], lS[3]+1):
+				spots.append([ x, y ]) # Creates the matrix of the parking coordinates
 
-		if _x == l:
-			spot_x = spots[0][0]
-			spot_y = spots[0][1]
-		else:
-			spot_x = spots[1][0]
-			spot_y = spots[1][1]
+		print spots
 
-		# for x in range(spot_x, _x):
-		# 	if [x, spot_y] not in spots:
-		# 		spots.insert(count, [x, spot_y])
-		# 	for y in range(spot_y, _y):
-		# 		if [y, spot_y] not in spots:
-		# 			spots.insert(count, [y, spot_y])
-		# 	count += 1
+		# These two is where the coordinates before the parking spot is stored
+		# Basically, these edges will be used to determine if the parking Spot has no obstacles for the car to enter
+		# If there is an obstacle then it cannot be parked
+		_edgeX = []
+		_edgeY = []
 
-		# for x in range(spots[0][0], cD[0]+1):
-		# 	if [spots[0][0], x] not in spots:
-		# 		spots.insert(count, [spots[0][0], x])
-		# 	for y in range(spots[0][1], cD[1]+1):
-		# 		if [y, x] not in spots:
-		# 			spots.insert(count, [y, x])
-		# 	count += 1
+		for edges in spots:
+			if edges[0] not in _edgeX:
+				_edgeX.append(edges[0]) # Gets all the points that are used as a coordinate in the x-axis
+			if edges[1] not in _edgeY:
+				_edgeY.append(edges[1]) # Gets all the points that are used as a coordinate in the y-axis
 
-		# for y in range(spots[0][1], cD[1]):
-		# 	if [y, x] not in spots:
-		# 		spots.insert(count, [y, x])
-		# 	for x in range(spots[0][1], cD[1]):
-		# 		if [spots[0][0], x] not in spots:
-		# 			spots.insert(count, [spots[0][0], x])
-		# 	count += 1
 
-		# while(len(spots) < numOfSpots):
-		# 	# spots.insert(count, [])
-		# 	# TODO: know the increment of the coordinates, WHICH IS WHICH?
-		# 	count += 1
+		# START, determines if there is an obstacle for the parking
+		# PLEASE take note that the parking spot can be entered after and before the coordinates ===== (4,5)[after] and (4, 0)[before]
 
-		# print spots
+		# If the condition is 1, then most likely it is parked vertically, making the y-axis points a very important factor for the spot
+		if iP[1] == 1: # If the luckySpot condition is 1 ======  lS[2] - lS[0] + 1 == cD[0] and lS[3] - lS[1] + 1 == cD[1]
+			for y in _edgeY:
+				_list = []
+				for x in range(min(_edgeX), 0, -1): # What this does is, it checks every point on the y-axis before the parking spot if there is an obstacle
+					if parkingLot[x-1][y]:
+						_list.append([x-1, y])
 
+				for x in range(max(_edgeX)+1, len(parkingLot)): # What this does is, it checks every point on the y-axis after the parking spot if there is an obstacle
+					if parkingLot[x][y]:
+						_list.append([x, y])
+
+				if len(_list) > 1: # If there is an obstacle both before and after then the parking spot is not parkable
+					print 0
+					return 0
+		# If the condition is 0, then most likely it is parked horizontally, making the x-axis points a very important factor for the spot
+		else:  # If the luckySpot condition is 1 ======  lS[2] - lS[0] + 1 == cD[1] and lS[3] - lS[1] + 1 == cD[0]
+			for x in _edgeX:
+				_list = []
+				for y in range(min(_edgeY), 0, -1):  # What this does is, it checks every point on the x-axis before the parking spot if there is an obstacle
+					if parkingLot[x][y-1]:
+						_list.append([x, y-1])
+
+				for y in range(max(_edgeY)+1, len(parkingLot[x])):  # What this does is, it checks every point on the x-axis after the parking spot if there is an obstacle
+					if parkingLot[x][y]:
+						_list.append([x, y])
+
+				if len(_list) > 1: # If there is an obstacle both before and after then the parking spot is not parkable
+					print 0
+					return 0
+		# END, determines if there is an obstacle for the parking
+
+		# Checks if every coordinate of the parking spot available
+		# If there is even a single obstacle then it is not parkable
 		for x in spots:
 			if parkingLot[x[0]][x[1]]:
 				print 0
 				return 0
 
+		# If every condition that makes the parking spot unparkable fails then it is safe to say that it can be parked
 		print 1
 		return 1
 
-	else:
+	else: # if luckySpot is not compatible with the carDimensions
 		return 0
 
-# # Output true
+# # Output false
 # parkingSpot(
-# 	[7, 2], 
+# 	[3, 2], 
 # 	[	
-# 		[0,1,0], 
-# 		[0,0,0], 
-# 		[0,0,0], 
-# 		[0,0,0], 
-# 		[0,0,0], 
-# 		[0,0,0], 
-# 		[0,0,0], 
-# 		[0,0,0]
+# 		[1,0,1,0,1,0], 
+# 		[1,0,0,0,1,0], 
+# 		[0,0,0,0,0,1], 
+# 		[1,0,0,0,1,1]
 # 	],
-# 	[1, 0, 7, 1]
+# 	[1, 1, 2, 3]
 # )
-# (7-1) + 1 = 7
-# (1-0) + 1 = 2
-# (7, 1)
-# (0, 1)
-# [1, 0], [7, 1]
-# [1, 0], [7, 0], [1, 1], [7, 1]
 
-_x = []
-array = [7, 2]
-array2 = [1, 0, 7, 1]
-# _x = []
-# array = [4, 1]
-# array2 = [0, 3, 3, 3]
+# # # # Output false
+parkingSpot(
+	[4, 1], 
+	[	
+		[1,1,1,1,1,1], 
+		[1,0,1,0,1,0], 
+		[1,0,0,0,1,0], 
+		[0,0,0,0,0,1], 
+		[1,0,0,0,1,1],
+		[1,1,1,0,1,1],
+	],
+	[1, 3, 4, 3]
+)
 
-# [[0, 3], [1, 3], [2, 3], [3, 3]]
-# [4, 1]
-# [0, 3, 3, 3]
-# cond 1
-# for x in range(0, 4):
-# 	for y in range(3, 4):
-# 		_x.append([x, y])
-
-# [7, 2]
-# [1, 0, 7, 1]
-# [1, 0], [7, 0], [1, 1], [7, 1]
-# cond 1
-# for x in range(1, 8):
-# 	for y in range(0, 2):
-# 		_x.append([ x, y ])
-
-# [1, 1], [1, 2], [1, 3]
-# [2, 1], [2, 2], [2, 3]
-# [3, 2], 
-# [1, 1, 2, 3]
-# cond 2
-# for x in range(1, 3):
-# 	for y in range(1, 4):
-# 		_x.append([ x, y ])
-
-# [7, 2],
-# [1, 1, 2, 7]
-# [1, 1], [1, 7], [2, 1], [2, 7]
-# cond 2
-# for x in range(1, 3):
-# 	for y in range(1, 8):
-# 		_x.append([ x, y ])
-
-# [2, 1], 
-# [0, 1, 1, 1]
-# [0, 1], [1, 1]
-# cond 1
-for x in range(0, 2):
-	for y in range(1, 2):
-		_x.append([ x, y ])
-
-print _x
-
-# # # Output true
+# # # # Output true
 # parkingSpot(
 # 	[4, 1], 
 # 	[	
@@ -154,16 +128,16 @@ print _x
 # 	],
 # 	[0, 3, 3, 3]
 # )
-# (3-0) + 1 = 4
-# (3-3) + 1 = 1
-# (0, 3)
-# (3, 3)
+# # (3-0) + 1 = 4
+# # (3-3) + 1 = 1
+# # (0, 3)
+# # (3, 3)
 
-# [0, 3], [1, 3], [2, 3], [3, 3]
+# # [0, 3], [1, 3], [2, 3], [3, 3]
 
-# [0, 3], [3, 3]
+# # [0, 3], [3, 3]
 
-# # Output true
+# # # Output true
 # parkingSpot(
 # 	[7, 2], 
 # 	[	
@@ -178,14 +152,14 @@ print _x
 # 	],
 # 	[1, 0, 7, 1]
 # )
-# (7-1) + 1 = 7
-# (1-0) + 1 = 2
-# (7, 1)
-# (0, 1)
-# [1, 0], [7, 1]
-# [1, 0], [7, 0], [1, 1], [7, 1]
+# # (7-1) + 1 = 7
+# # (1-0) + 1 = 2
+# # (7, 1)
+# # (0, 1)
+# # [1, 0], [7, 1]
+# # [1, 0], [7, 0], [1, 1], [7, 1]
 
-# # Output true
+# # # Output true
 # parkingSpot(
 # 	[3, 2], 
 # 	[
@@ -196,15 +170,15 @@ print _x
 # 	],
 # 	[1, 1, 2, 3]
 # )
-# (2-1) + 1 = 2
-# (3-1) + 1 = 3
+# # (2-1) + 1 = 2
+# # (3-1) + 1 = 3
 
-# [1, 1], [1, 2], [1, 3]
-# [2, 1], [2, 2], [2, 3]
+# # [1, 1], [1, 2], [1, 3]
+# # [2, 1], [2, 2], [2, 3]
 
-# [1, 1], [2, 3]
+# # [1, 1], [2, 3]
 
-# # # Output true
+# # # # Output true
 # parkingSpot(
 # 	[2, 1], 
 # 	[	
@@ -214,13 +188,13 @@ print _x
 # 	],
 # 	[0, 1, 1, 1]
 # )
-# (1-0) + 1 = 2
-# (1-1) + 1 = 1
-# (0, 2)
-# (0, 1)
-# [0, 1], [1, 1]
+# # (1-0) + 1 = 2
+# # (1-1) + 1 = 1
+# # (0, 2)
+# # (0, 1)
+# # [0, 1], [1, 1]
 
-# # Output true
+# # # Output true
 # parkingSpot(
 # 	[2, 1],
 # 	[	
@@ -230,13 +204,13 @@ print _x
 # 	],
 # 	[1, 2, 1, 3]
 # )
-# [1-1] + 1 = 1
-# [3-2] + 1 = 2
-# (1, 1)
-# (2, 3)
-# [1, 2], [1, 3]
+# # [1-1] + 1 = 1
+# # [3-2] + 1 = 2
+# # (1, 1)
+# # (2, 3)
+# # [1, 2], [1, 3]
 
-# # Output true
+# # # Output true
 # parkingSpot(
 # 	[7, 2],
 # 	[	
@@ -246,25 +220,13 @@ print _x
 # 	],
 # 	[1, 1, 2, 7]
 # )
-# (2-1) + 1 = 2
-# (7-1) + 1 = 7
-# (0, 3)
-# (3, 3)
-# [1, 1], [1, 7], [2, 1], [2, 7]
+# # (2-1) + 1 = 2
+# # (7-1) + 1 = 7
+# # (0, 3)
+# # (3, 3)
+# # [1, 1], [1, 7], [2, 1], [2, 7]
 
-# # Output false
-# parkingSpot(
-# 	[3, 2], 
-# 	[	
-# 		[1,0,1,0,1,0], 
-# 		[1,0,0,0,1,0], 
-# 		[0,0,0,0,0,1], 
-# 		[1,0,0,0,1,1]
-# 	],
-# 	[1, 1, 2, 3]
-# )
-
-# # Output false
+# # # Output false
 # parkingSpot(
 # 	[2, 1],
 # 	[	
@@ -274,12 +236,12 @@ print _x
 # 	],
 # 	[1, 1, 2, 1]
 # )
-# (2-1) + 1 = 2
-# (1-1) + 1 = 1
-# (1, 2)
-# (1, 1)
+# # (2-1) + 1 = 2
+# # (1-1) + 1 = 1
+# # (1, 2)
+# # (1, 1)
 
-# # Output false
+# # # Output false
 # parkingSpot(
 # 	[2, 1],
 # 	[	
@@ -289,12 +251,12 @@ print _x
 # 	],
 # 	[0, 1, 1, 1]
 # )
-# (1-0) + 1 = 2
-# (1-1) + 1 = 1
-# (0, 1)
-# (1, 1)
+# # (1-0) + 1 = 2
+# # (1-1) + 1 = 1
+# # (0, 1)
+# # (1, 1)
 
-# # Output false
+# # # Output false
 # parkingSpot(
 # 	[2, 1],
 # 	[	
@@ -304,12 +266,12 @@ print _x
 # 	],
 # 	[2, 1, 2, 2]
 # )
-# (2-1) + 1 = 1
-# (2-1) + 1 = 2
-# (2, 2)
-# (1, 2)
+# # (2-1) + 1 = 1
+# # (2-1) + 1 = 2
+# # (2, 2)
+# # (1, 2)
 
-# # # Output false
+# # # # Output false
 # parkingSpot(
 # 	[4, 2], 
 # 	[	
@@ -320,7 +282,7 @@ print _x
 # 	[0, 0, 1, 3]
 # )
 
-# # Output false
+# # # Output false
 # parkingSpot(
 # 	[7, 2], 
 # 	[	
@@ -336,7 +298,7 @@ print _x
 # 	[1, 0, 7, 1]
 # )
 
-# # Output false
+# # # Output false
 # parkingSpot(
 # 	[5, 3],
 # 	[	
